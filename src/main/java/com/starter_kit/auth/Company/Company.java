c57e0ac6652bf4c9eea3dfd0686d90742e9bb2a5
Fernando Boza <hi@fernandoboza.com>
2019-09-08 22:11:35 -0400

    securing gits

---
+++ src/main/java/com/starter_kit/auth/Company/Company.java
@@ -0,0 +1,9 @@
+package com.starter_kit.auth.Company;
+
+import java.util.List;
+
+public class Company {
+    public long id;
+    public String name;
+    public List<Long> usersId;
+}
---
+++ src/main/java/com/starter_kit/auth/Users/User.java
@@ -0,0 +1,8 @@
+package com.starter_kit.auth.Users;
+
+public class User {
+    public long id;
+    public String fName;
+    public String lName;
+    public long companyID;
+}
